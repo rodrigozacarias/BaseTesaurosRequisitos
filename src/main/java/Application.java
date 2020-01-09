c5456616ba896aab6d4e42444be4b3853558cb4b
rozacarias <rodrigozacarias@id.uff.br>
2020-01-09 09:28:19 -0300

    Authentication

---
+++ src/main/java/Application.java
@@ -0,0 +1,15 @@
+import com.requirementsthesauri.service.Authentication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        Authentication authentication = new Authentication();
+        authentication.getConnectionDataBase();
+    }
+
+}
