ff21b839822165e698742b07a8613a7c6329f7df
rozacarias <rodrigozacarias@id.uff.br>
2020-01-16 16:22:57 -0300

    Tentando fazer a tela de criar domínio

--- src/main/java/com/requirementsthesauri/controller/webapp/WebAppDomainController.java
+++ src/main/java/com/requirementsthesauri/controller/webapp/WebAppDomainController.java
@@ -1,11 +1,16 @@
 package com.requirementsthesauri.controller.webapp;
 
+import com.requirementsthesauri.model.Domain;
 import com.requirementsthesauri.service.DomainService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("webapp/domains")
@@ -21,13 +26,33 @@ public class WebAppDomainController {
         return new ModelAndView("domain");
     }
 
-    @DeleteMapping(value = "/excluir")
-    public ModelAndView deleteDomain(@PathVariable(value="domainURI") String domainURI, Model model) throws Exception {
+    @PostMapping("/getAllDomains")
+    public ModelAndView deleteDomain(@RequestParam(value="uriDomain") String domainURI, Model model) throws Exception {
         domainService.deleteDomain(domainURI);
 
         model.addAttribute("domains", domainService.getAllDomains());
 
-        return new ModelAndView("redirect: domain");
+        return new ModelAndView("redirect:getAllDomains");
     }
 
+    @GetMapping("/create")
+    public ModelAndView createDomain(Model model) throws Exception {
+
+        return new ModelAndView("createDomain");
+    }
+
+    @PostMapping("/createDomain")
+    public ModelAndView createNewDomain(@ModelAttribute Domain domain, BindingResult bindingResult, Model model) throws Exception {
+
+        if (bindingResult.hasErrors()) {
+            //errors processing
+        }
+        model.addAttribute("domain", domain);
+
+        List<Domain> domains  = new ArrayList<>();
+        domains.add(domain);
+        domainService.createDomain(domains);
+
+        return new ModelAndView("redirect:getAllDomains");
+    }
 }
